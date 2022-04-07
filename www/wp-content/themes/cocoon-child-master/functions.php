210b01a746c8a47dbe8f338ad35e8da9ea9c5f01
yuki_ueno <yuki_ueno@xonor.jp>
2022-04-07 13:02:43 +0900

    feat : supported Easy Fancybox to eye-catch

--- www/wp-content/themes/cocoon-child-master/functions.php
+++ www/wp-content/themes/cocoon-child-master/functions.php
@@ -1,10 +1,13 @@
 <?php //子テーマ用関数
 if ( !defined( 'ABSPATH' ) ) exit;
 
+//子テーマ用のビジュアルエディタースタイルを適用
+add_editor_style();
+
+//以下に子テーマ用の関数を書く
 /* ====================================
  * パスワード保護に関する設定 
  * ==================================== */
-
 // パスワード保護ページの「保護中:」を消す
 add_filter('protected_title_format', 'remove_protected');
 function remove_protected($title) { return '%s'; }
@@ -13,7 +16,5 @@ function remove_protected($title) { return '%s'; }
 add_action('after_setup_theme', 'my_after_setup_theme' );
 function my_after_setup_theme() { setcookie('wp-postpass_' . COOKIEHASH, $_POST['post_password'], 0, COOKIEPATH); };
 
-//子テーマ用のビジュアルエディタースタイルを適用
-add_editor_style();
-
-//以下に子テーマ用の関数を書く
+add_theme_support('post_thumbnails');
+set_post_thumbnail_size(160, 120, true);
